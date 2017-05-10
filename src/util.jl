ccaae00c40e9e9557d7fdc45cad1d78d4f41bc39
Andy Nowacki <a.nowacki@leeds.ac.uk>
2017-05-10 17:14:57 +0100

    Add more sample data
    Include all the sample data distributed with SAC/BRIS.  Load these
    data by calling `SAC.sample()` with an argument, which should be
    a `Symbol` describing which dataset.  See the help for `SAC.sample`
    for details.

--- src/util.jl
+++ src/util.jl
@@ -15,15 +15,46 @@ isundefined(x::SACChar) = strip(x) == strip(sac_cnull)
 isundefined(x::SACBool) = false
 isundefined(s::SACtr, x::Symbol) = isundefined(getfield(s, x))
 
+"Directory containing sample data"
+const SAMPLE_DATA_DIR = joinpath(dirname(@__FILE__()), "..", "data")
+
 """
     sample() -> ::SACtr
+    sample(kind::Symbol) -> ::Array{SACtr}
 
-Return some sample SAC data, a regional earthquake arrival.
+Return some sample SAC data.
+
+With no arguments, `sample` gives one trace from a local earthquake recorded in
+California.
+
+In the second form, a set of traces is returned according to the table below:
+
+|`kind`|Description|
+|:-----|:----------|
+|`:local`|Livermore Valley, CA.  9 3-component stations|
+|`:regional`|Nevada.  4 3-component stations|
+|`:teleseism`|**Mid-period** recording of Eureka, CA event.  4 3-c stations|
+|`:teleseisl`|**Long-period** recording of Eureka, CA event.  4 3-c stations|
+|`:array`|Deep Fiji event.  60 vertical stations in the UK|
 """
 function sample()
-    file = joinpath(dirname(@__FILE__()), "../data/seis.sac")
-    return SAC.read(file)
+    file = joinpath(SAMPLE_DATA_DIR, "seis.sac")
+    SAC.read(file)
 end
+const SAMPLE_DATA_KINDS = [:local, :regional, :teleseism, :teleseisl, :array]
+function sample(kind::Symbol)
+    file_pattern = "*"
+    kind in SAMPLE_DATA_KINDS ||
+        throw(ArgumentError("Sample data kind must be one of: $SAMPLE_DATA_KINDS"))
+    dir = joinpath(SAMPLE_DATA_DIR, string(kind))
+    if kind in [:teleseism, :teleseisl]
+        file_pattern = "*" * dir[end] * ".*"
+        dir = dir[1:end-1]
+    end
+    A, f = read_wild(file_pattern, dir, echo=false)
+    A
+end
+
 
 """
     time(::SACtr) -> t
