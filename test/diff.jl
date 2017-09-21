cab5a809de1dfa85eaa29c09c0a9ce2c267d48f3
Andy Nowacki <a.nowacki@leeds.ac.uk>
2017-09-21 11:49:34 +0100

    Encapsulate tests in testsets

--- test/diff.jl
+++ test/diff.jl
@@ -1,18 +1,19 @@
 # Test the differentiate function
 using SAC, Base.Test
 
-sample_data = SAC.sample()
+@testset "Differentiation" begin
+    sample_data = SAC.sample()
 
-# Don't accept wrong arguments
-for i in (0, 1, 4, 6)
-    @test_throws ArgumentError SAC.differentiate(sample_data, i)
+    # Don't accept wrong arguments
+    for i in (0, 1, 4, 6)
+        @test_throws ArgumentError SAC.differentiate(sample_data, i)
+    end
+
+    # Compare against output from SAC
+    for (i, str) in zip((2, 3, 5), ("two", "three", "five"))
+        file = joinpath(dirname(@__FILE__), "data", "test_diff_data_points_$(str).sac")
+        isfile(file) || error("Test data file '$(file)' for differentiate does not exist")
+        reference_data = SAC.read(file)
+        @test SAC.differentiate(sample_data, i).t ≈ reference_data.t
+    end
 end
-
-# Compare against output from SAC
-for (i, str) in zip((2, 3, 5), ("two", "three", "five"))
-    file = joinpath(dirname(@__FILE__), "data", "test_diff_data_points_$(str).sac")
-    isfile(file) || error("Test data file '$(file)' for differentiate does not exist")
-    reference_data = SAC.read(file)
-    @test SAC.differentiate(sample_data, i).t ≈ reference_data.t
-end
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-include("io.jl")
-include("diff.jl")
-include("integrate.jl")
-include("stack.jl")
-include("great_circle.jl")
-include("rotate_to_gcp.jl")
+using Base.Test
+
+@testset "SAC tests" begin
+    include("construction.jl")
+    include("io.jl")
+    include("diff.jl")
+    include("integrate.jl")
+    include("stack.jl")
+    include("great_circle.jl")
+    include("rotate_to_gcp.jl")
+end
